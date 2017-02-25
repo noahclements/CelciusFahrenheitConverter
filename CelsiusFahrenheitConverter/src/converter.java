3e22c3624461b1dabe64a4b2e190571bb22bccd6
Noah Clements <noahnoah900@gmail.com>
2017-02-25 00:45:49 -0400

    working

--- CelsiusFahrenheitConverter/src/converter.java
+++ CelsiusFahrenheitConverter/src/converter.java
@@ -5,60 +5,44 @@ import java.io.InputStreamReader;
 class CelciusFahrenheitConverter {
 	double celcius;
 	double fahrenheit;
-	
-	
+
 	void CeltoFah() {
-		fahrenheit = ((9.0 / 5.0) * celcius + 32);
+		System.out.println("Your converted celcius degrees is: " + celcius + " degrees fahrenheit.");
+		return;
 	}
-	
+
 	void FahtoCel() {
-		celcius  = (5.0 / 9.0) * (fahrenheit - 32);
+		System.out.println("Your converted celcius degrees is: " + fahrenheit + " degrees celcius.");
+		return;
 	}
-	
-	
-	
-	
-	
-	
+
 }
-	
-
-
-
-
-
-
-
 
 public class converter {
 	public static void main(String[] args) throws IOException {
 		InputStreamReader inStream = new InputStreamReader(System.in);
 		BufferedReader stdin = new BufferedReader(inStream);
-		String inData;	// declares data input String as inData
-		int input;
-		
+		String inData; // declares data input String as inData
+		double input;
+
 		CelciusFahrenheitConverter conversion = new CelciusFahrenheitConverter();
-		
+
 		System.out.println("Are you converting celcius or fahrenheit");
 		inData = stdin.readLine();
-			
-		
-		if(inData.equalsIgnoreCase("celcius")) {
+
+		if (inData.equalsIgnoreCase("celcius")) {
 			System.out.println("Please enter the degrees in celcius.");
 			inData = stdin.readLine();
-			conversion.celcius = Integer.parseInt(inData);
-			
+			input = Double.parseDouble(inData);
+			conversion.celcius = ((9.0 / 5.0) * input + 32);
 			conversion.CeltoFah();
-		} else if(inData == "fahrenheit") {
+		} else if (inData.equalsIgnoreCase("fahrenheit")) {
 			System.out.println("Please enter the degrees in celcius.");
-			input = Integer.parseInt(inData);
-			conversion.fahrenheit = Integer.parseInt(inData);
+			inData = stdin.readLine();
+			input = Double.parseDouble(inData);
+			conversion.fahrenheit = (5.0 / 9.0) * (input - 32);
 			conversion.FahtoCel();
 		}
-		
-		
-		
-		
-		
+
 	}
 }
